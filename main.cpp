ae5687143ee13dd8c0df57ae48f9bd064340656c
peridotanemone <sophia@pronto-tech.com>
2024-05-22 08:36:53 -0400

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+	array<int, 4> inputs;
+	cout << "Type in four integers: \n";
+	for (int i = 0; i < 4; i++) { cin >> inputs[i]; }
+
+
+
+	return 0;
+}
