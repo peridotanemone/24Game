28555ad3d4bd0e34304b210ca7f84c915bf09ddf
peridotanemone <sophia@pronto-tech.com>
2024-05-22 09:58:49 -0400

    if you don't run it, it works!

--- main.cpp
+++ main.cpp
@@ -1,11 +1,52 @@
 #include <iostream>
+#include <deque>
 #include <array>
 
 using namespace std;
+constexpr array<char, 4> operators{'+','-','*','/'};
+
+int evaluate(int x, int y, char op)
+{
+	switch (op)
+	{
+	case '+':
+		return (x + y);
+
+	case '-':
+		return (x - y);
+
+	case '*':
+		return (x * y);
+
+	case '/':
+		return (x / y);
+
+	default: return -1;
+	}
+}
+
+void solve(deque<int> &arr, int s_index, int l_index)
+{
+	if ((arr[0] == 24) && (arr.size() == 1))
+	{
+		return;
+	}
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			if (arr.size() > 0)
+			{
+				arr[1] = evaluate(arr[s_index], arr[l_index], operators[j]);
+				arr.pop_front();
+				solve(arr, s_index + i, l_index + i);
+
+			}
+		}
+	}
+}
 
 int main()
 {
-	array<int, 4> inputs;
+	deque<int> inputs;
 	cout << "Type in four integers: \n";
 	for (int i = 0; i < 4; i++) { cin >> inputs[i]; }
 
