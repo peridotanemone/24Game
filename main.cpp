f21fdbaa200416cf4769ff7025e0ba8cbbed0cfd
peridotanemone <sophia@pronto-tech.com>
2024-05-23 09:00:55 -0400

    medium well

--- main.cpp
+++ main.cpp
@@ -41,7 +41,7 @@ void solve(deque<int>& arr, deque<string>& moves)
 			deque<int> l;
 			copy(arr.begin(), arr.end(), back_inserter(l));
 			int n = evaluate(arr[0], arr[1], operators[i]);
-			string output = to_string(arr[0]) + " " + operators[i] + " " + to_string(arr[1]) + "\n";
+			string output = to_string(arr[0]) + " " + operators[i] + " " + to_string(arr[1]) + " = " + to_string(n) + "\n";
 			moves.push_back(output);
 			arr.pop_front();
 			arr.pop_front();
@@ -54,13 +54,32 @@ void solve(deque<int>& arr, deque<string>& moves)
 	return;
 }
 
+void createPermutations(deque<int>& in, deque<deque<int>>& out, int l, int r)
+{
+	if (l == r)
+	{
+		out.push_back(in);
+		return;
+	}
+	for (int i = l; i < r; i++)
+	{
+		swap(in[l], in[i]);
+
+		createPermutations(in, out, l + 1, r);
+
+		swap(in[i], in[l]);
+	}
+}
+
 int main()
 {
 	deque<int> inputs;
+	deque<deque<int>> permutations;
 	inputs.resize(4);
 	deque<string> moves{};
 	cout << "Type in four integers: \n";
 	for (int i = 0; i < 4; i++) { cin >> inputs[i]; }
-	solve(inputs, moves);
+	createPermutations(inputs, permutations, 0, 3);
+	for (auto e : permutations) { solve(e, moves); }
 	return 0;
 }
